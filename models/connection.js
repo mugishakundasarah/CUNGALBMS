83da8ca760904971381f0aafca367fe69c4c5dc8
sarah <mugishakundasarah@gmail.com>
2021-02-25 10:04:11 +0200

    well configure the db

--- models/connection.js
+++ models/connection.js
@@ -5,7 +5,7 @@ dotenv.config()
 
 const mongoConnect = async() => {
     try {
-        await mongoose.connect(process.env.DATABASE, {useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true}, ()=> {
+        await mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true}, ()=> {
             console.log('database connected')
         })
     } catch (error) {
