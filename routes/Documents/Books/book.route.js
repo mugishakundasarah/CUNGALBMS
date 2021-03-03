820e42f68d86d5ef25a6f6465d253b783845bb07
graziee6 <gngrace10@gmail.com>
2021-03-03 18:54:15 +0200

    added book get documentation

--- routes/Documents/Books/book.route.js
+++ routes/Documents/Books/book.route.js
@@ -11,7 +11,7 @@ router.route('/')
      *     description: Add books
      *     parameters:
      *       - name: body
-     *         description: Fields for staff
+     *         description: Entries for books
      *         in: body
      *         required: true
      *         schema: 
@@ -22,20 +22,20 @@ router.route('/')
      *             total:
      *               type: string
      *               description: Total of the same title 
-     *               unitPrice:
+     *             unitPrice:
      *               type: string
      *               description: One book's cost of its type
      *             publisher: 
      *               type: string
      *               description: The book's publishing agency
-     *            publishingDate:
+     *             publishingDate:
      *              type: Date()
      *              description:  If known,the date of the book's publication
-     *            category_id:
+     *             category_id:
      *              type:mongoose.Schema.Types.objectId
      *              ref:"docTypeSchema"
      *              description: In identifier of its type
-     *            status:
+     *             status:
      *              type:string
      *              default: 'Kept'
      *              enum:['Kept', 'At risk', 'Lost']
@@ -45,7 +45,29 @@ router.route('/')
      *         description: OK
      */
 .post(postBooks)
+
+/**
+ * @swagger
+ * /books/:
+ *   get: 
+ *     tags:
+ *       - Books
+ *     description: get books from the database
+ *     parameters:
+ *       - name: page
+ *         description: page number
+ *         in: query
+ *         type: string
+ *       - name: limit
+ *         description: elements per page
+ *         in: query
+ *         type: string
+ *     responses: 
+ *       200:
+ *         description: OK
+ */
 .get(getAllBooks)
+
 .put(updateBook)
 .delete(deleteBook)
 module.exports.bookRoutes = router
